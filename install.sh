ad0e3b9b125de32e0abde3cb38b2ba2f22cc5ec5
roger-turowski <96268886+roger-turowski@users.noreply.github.com>
2025-03-28 16:23:43 -0400

    Added some disk intitializing commands.

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@
 echo "Hello world!"
 
 # Start here...
+sgdisk --zap-all --clear /dev/sda
+sgdisk --new=1:0:+550M --typecode=1:ef00 --change-name=1:EFI /dev/sda
+sgdisk --new=2:0:0 --typecode=2:8300 --change-name=2:root /dev/sda
+mkfs.fat -F 32 /dev/sda1
