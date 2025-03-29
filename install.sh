cf16882eb1060835e17f02370108795db5f998da
roger-turowski <96268886+roger-turowski@users.noreply.github.com>
2025-03-29 04:22:59 -0400

    Added commands to partition and format and tested on Arch VM.

--- install.sh
+++ install.sh
@@ -10,26 +10,42 @@ echo "Hello world!"
 # Clear the disk
 sgdisk --zap-all --clear /dev/sda
 
-# Create the EFI partition
-sgdisk --new=1:0:+550M --typecode=1:ef00 --change-name=1:EFI /dev/sda
+# PHYSICAL PARTITIONS
 
-# Format the EFI partition
-mkfs.fat -F 32 /dev/sda1
+# Create the physical EFI partition
+sgdisk --new=1:0:+1G --typecode=1:ef00 --change-name=1:EFI /dev/sda
 
 # Create the physical partition for root, swap and home
 ### pvcreate /dev/sda (not working)
-sgdisk --new=2:0:0 --typecode=2:8503 --change-name=2:root /dev/sda
+sgdisk --new=2:0:0 --typecode=2:8e00 --change-name=2:root /dev/sda
+
+# PHYSICAL VOLUMES
+
+# Create a physical volume to contain the volume group "system"
+pvcreate /dev/sda2
+
+# VOLUME GROUPS
 
 # Create the volume group for root, swap and home
-vgcreate system /dev/sda
+vgcreate system /dev/sda2
+
+# LOGICAL VOLUMES 
 
 # Create the logical volumes for root, swap and home
-lvcreate -L 20%FREE -n root system
+lvcreate -l 20%FREE -n root system
 lvcreate -L 2G -n swap system
 lvcreate -L 100%FREE -n home system
 
+# FORMAT THE PARTITIONS
+
+# Format the EFI partition
+mkfs.fat -L EFI -F32 /dev/sda1
+
 # Format the root volume with BTRFS
-mkfs.btrfs /dev/system/root
+mkfs.btrfs -L root /dev/system/root
 
 # Format the home volume with ext4
-mkfs.ext4 /dev/system/home
+mkfs.ext4 -L home /dev/system/home
+
+# Create swap space
+mkswap -L swap /dev/system/swap
