681c4bbd76e9a4de90d726ec248aec218e95e616
roger-turowski <96268886+roger-turowski@users.noreply.github.com>
2026-08-28 23:00:34 -0400

    Removed unnecessary pause.

--- install.sh
+++ install.sh
@@ -1374,11 +1374,8 @@ main() {
   arch-chroot $my_root_mount useradd -c "$my_full_name" -mG wheel -s $my_shell -p "$my_password_hash" $my_user_id
 
   if [ "$install_podman_pkgs" -eq 0 ]; then
-    set -x
     log_info "Installing Podman packages"
     arch-chroot $my_root_mount pacman -S --needed --noconfirm --quiet "${podman_pkgs[@]}"
-    set +x
-    pause
   fi
 
   if [ "$install_gui_apps" -eq 0 ]; then
